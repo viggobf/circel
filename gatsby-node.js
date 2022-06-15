19fba8aedc63a1a2cfce8d3339be767c0dfc8b65
Viggo Bryant-Frost <viggobryantfrost@gmail.com>
2022-06-15 17:02:44 +0100

    testidkmassivenumberlol

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
     if (stage === 'build-html') {
       actions.setWebpackConfig({
-        externals: getConfig().externals.concat(({context, request}, cb) => {
+        externals: getConfig().externals.concat(({context, request}, callback) => {
           const regex = /^@?firebase(\/(.+))?/
           // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
           if (regex.test(request)) {
             return callback(null, `commonjs ${request}`) // <- use commonjs!
           }
-          cb()
+          callback()
         }),
       })
     }
